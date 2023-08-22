9286606a8435f2033ccb585a9e25161b8ea5c140
JSSimola <jssimola@gmail.com>
2023-08-22 14:28:04 +0300

    Moduuli 2 Tehtävä 6

--- Moduuli02/Random_Numbers.py
+++ Moduuli02/Random_Numbers.py
@@ -1,21 +1,26 @@
 import random
 
-# 3 numeroinen koodi
+# Ei saa nollaa ekaksi luvuksi :(
+#def digitgen(n):
+#    min = 10 ** (n-1)
+#   max = 10 ** n-1
+#    return random.randint(min, max)
 
-def digitgen(n):
-    min = 10 ** (n-1)
-    max = 10 ** n-1
-    return random.randint(min, max)
+def digitgen1(n_1):
+    numbers = "0123456789"
+    code = ""
+    for i in range(0,n_1):
+        code+= random.choice(numbers)
+    return code
 
 def digitgen2(n_2):
-    numbers = "0123456"
+    numbers = "123456"
     code = ""
-    for _ in range(0,n_2):
+    for i in range(0,n_2):
         code+= random.choice(numbers)
     return code
 
 print("Kolmen numeron koodi:")
-print(digitgen(3))
+print(digitgen1(3))
 print("Neljän numeron koodi:")
 print(digitgen2(4))
-
